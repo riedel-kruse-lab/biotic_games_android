47295adb2345b8f3d53e19aeafeee8feca20d31f
Daniel Chiu <daniechiu@gmail.com>
2015-01-06 23:41:01 -0800

    Documents MainActivity.java.

--- app/src/main/java/edu/stanford/riedel_kruse/bioticgames/MainActivity.java
+++ app/src/main/java/edu/stanford/riedel_kruse/bioticgames/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 /**
- * Created by dchiu on 12/7/14.
+ * Main activity for the application. Displays the menu screen and launches other activities.
  */
 public class MainActivity extends Activity
 {
@@ -17,12 +17,20 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Starts the GameActivity. Activated by button press (see activity_main.xml).
+     * @param view
+     */
     public void startGame(View view)
     {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Starts the GameActivity in tutorial mode. Activated by button press (see activity_main.xml)
+     * @param view
+     */
     public void startTutorial(View view)
     {
         Intent intent = new Intent(this, GameActivity.class);
