bedfaa7096782c0e133d880abeaca3f650e08fe9
Daniel Chiu <daniechiu@gmail.com>
2015-03-03 11:46:35 -0800

    Moves top buttons to main screen.

--- app/src/main/java/edu/stanford/riedel_kruse/bioticgames/GameActivity.java
+++ app/src/main/java/edu/stanford/riedel_kruse/bioticgames/GameActivity.java
@@ -465,22 +465,6 @@ public class GameActivity extends BioticGameActivity implements SoccerGameDelega
         updateScoreView();
     }
 
-    public void onInstructionsPressed(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Instructions");
-        builder.setMessage("Try to score the ball into the other player's goal to win! " +
-                        "You get 2 points for carrying the ball into the goal, and 1 point for passing it in.\n\n" +
-                        "Control the Euglena with the joystick, and pass the ball by tapping the Pass button. \n\n" +
-                        "When time runs out, there is a short pause to hand off the controller to the other player."
-        );
-        builder.setCancelable(false);
-        builder.setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            }
-        });
-        builder.show();
-    }
-
     @Override
     protected void initGame(int width, int height) {
         mSoccerGame = new SoccerGame(width, height, 5, this);
@@ -538,36 +522,6 @@ public class GameActivity extends BioticGameActivity implements SoccerGameDelega
         drawScaleBar(frame);
 
         playSoundEffects();
-    }
-
-    public void infoButtonPressed(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("About Euglena:");
-        builder.setMessage("Euglena are a single-celled, photosynthetic organism! " +
-                        "Euglena can be controlled by light simuli. Can you tell if the Euglena seek or avoid the light?"
-        );
-        builder.setCancelable(false);
-        builder.setPositiveButton("I feel smarter already!", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            }
-        });
-        builder.show();
-    }
-
-    public void creditsButtonPressed(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Credits");
-        builder.setMessage("Honesty Kim: Messing Things Up\nDaniel Chiu: Programming\n" +
-                        "Seung Ah Lee: Optics\nAlice Chung: Euglena Biology\nSherwin Xia: Electronics\n" +
-                        "Lukas Gerber: Sticker Microfluidics\nNate Cira: Microfluidics\n" +
-                        "Ingmar Riedel-Kruse: Advisor"
-        );
-        builder.setCancelable(false);
-        builder.setPositiveButton("Good job guys!", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            }
-        });
-        builder.show();
     }
 
     public void blinkingArrow(Mat img) {
--- app/src/main/java/edu/stanford/riedel_kruse/bioticgames/MainActivity.java
+++ app/src/main/java/edu/stanford/riedel_kruse/bioticgames/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.stanford.riedel_kruse.bioticgames;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -29,4 +31,53 @@ public class MainActivity extends Activity
         intent.putExtra(GameActivity.EXTRA_TUTORIAL_MODE, true);
         startActivity(intent);
     }
+
+    public void onInstructionsPressed(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Instructions");
+        builder.setMessage("Try to score the ball into the other player's goal to win! " +
+                        "You get 2 points for carrying the ball into the goal, and 1 point for passing it in.\n\n" +
+                        "Control the Euglena with the joystick, and pass the ball by tapping the Pass button. \n\n" +
+                        "When time runs out, there is a short pause to hand off the controller to the other player."
+        );
+        builder.setCancelable(false);
+        builder.setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.show();
+    }
+
+
+    public void infoButtonPressed(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("About Euglena:");
+        builder.setMessage("Euglena are a single-celled, photosynthetic organism! " +
+                        "Euglena can be controlled by light simuli. Can you tell if the Euglena seek or avoid the light?"
+        );
+        builder.setCancelable(false);
+        builder.setPositiveButton("I feel smarter already!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.show();
+    }
+
+    public void creditsButtonPressed(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Credits");
+        builder.setMessage("Honesty Kim: Messing Things Up\nDaniel Chiu: Programming\n" +
+                        "Seung Ah Lee: Optics\nAlice Chung: Euglena Biology\nSherwin Xia: Electronics\n" +
+                        "Lukas Gerber: Sticker Microfluidics\nNate Cira: Microfluidics\n" +
+                        "Ingmar Riedel-Kruse: Advisor"
+        );
+        builder.setCancelable(false);
+        builder.setPositiveButton("Good job guys!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.show();
+    }
+
+
 }
